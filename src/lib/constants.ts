a304f97279d0fb506f48d40c22b46cadac58aa73
Caio <caiohportella@gmail.com>
2025-06-10 12:18:52 -0300

    chore: configura o projeto

---
+++ src/lib/constants.ts
@@ -0,0 +1,11 @@
+export const SUPPLIER_BR = {
+  ID: "BR" as const,
+  ENDPOINT: "https://616d6bdb6dacbb001794ca17.mockapi.io/devnology/brazilian_provider",
+};
+
+export const SUPPLIER_EU = {
+  ID: "EU" as const,
+  ENDPOINT: "https://616d6bdb6dacbb001794ca17.mockapi.io/devnology/european_provider",
+};
+
+export const PRODUCTS_PER_PAGE = 12;
---
+++ src/lib/types.ts
@@ -0,0 +1,25 @@
+export interface Product {
+  id: string;
+  name: string;
+  description: string;
+  imageUrl: string;
+  price: number;
+  source: "BR" | "EU";
+  category?: string;
+  department?: string;
+  adjective?: string;
+  material?: string;
+  images?: string[];
+  hasDiscount?: boolean;
+  discountValue?: number;
+}
+
+export type ProductFilter = {
+  category?: string;
+  department?: string;
+  source?: "BR" | "EU";
+  priceMin?: number;
+  priceMax?: number;
+  searchTerm?: string;
+  onSale?: boolean;
+};
