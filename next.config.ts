1acd6529294e3124d9f4d83312965031a719e9c1
Caio <caiohportella@gmail.com>
2025-06-10 12:17:30 -0300

    chore: configura o projeto

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "placeimg.com",
+      },
+      {
+        protocol: "https",
+        hostname: "loremflickr.com",
+      },
+    ]
+  }
 };
 
 export default nextConfig;
